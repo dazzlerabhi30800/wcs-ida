43fef7999a323ca4e554b74c7a88c8ab8876cd82
abhishekchoudharywcs <abhishek.dev@webconsultancyservices.in>
2024-09-17 20:38:03 +0530

    project gallery detail responsive

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -5,18 +5,19 @@ import { useGlobalContext } from "../context";
 const Gallery = () => {
   const { galleryData } = useGlobalContext();
   return (
-    <section className={`mt-[120px]`}>
-      <div className={`${styles.marginX} ${styles.flexCol} gap-14`}>
+    <section className={`mt-[40px] md:mt-[80px] lg:mt-[120px]`}>
+      <div className={`${styles.marginX} ${styles.flexCol} gap-10 md:gap-14`}>
         <h2 className={`${fontStyles.h2} font-bold text-blue`}>
           Project Gallery
         </h2>
         {/* Gallery */}
-        <div className={`grid grid-cols-4 grid-rows-[280px_280px] gap-4`}>
+        <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 md:grid-rows-[200px_200px] lg:grid-rows-[250px_250px] xl:grid-rows-[280px_280px] gap-4 sm:gap-3 md:gap-2 lg:gap-4`}>
           {galleryData?.map((gallery, index) => (
             <div className="galleryComp" key={index}>
               <img
                 src={gallery}
-                className="w-full h-full rounded-lg object-cover"
+                // className="w-full h-[300px] sm:h-[250px] md:h-[300px] lg:h-full rounded-lg object-cover"
+                className="w-full h-[300px] sm:h-[250px] md:h-full rounded-lg object-cover"
                 alt=""
               />
             </div>
--- src/Components/ProjectDetail.jsx
+++ src/Components/ProjectDetail.jsx
@@ -4,10 +4,10 @@ import Para1 from "../styles/Para1";
 
 const ProjectDetail = () => {
   return (
-    <section className={`mt-40`}>
-      <div className={`${styles.marginX} ${styles.flexCol} gap-6`}>
+    <section className={`${styles.marginT}`}>
+      <div className={`${styles.marginX} ${styles.flexCol} gap-8 md:gap-6`}>
         {/* Gallery */}
-        <div className="grid grid-cols-2 grid-rows-[327px_327px] gap-4">
+        <div className="grid grid-cols-2 grid-rows-[150px_150px] sm:grid-rows-[190px_190px] md:grid-rows-[327px_327px] gap-2 md:gap-4">
           <img
             src="./images/Random Imgs/img1.jpg"
             className={`w-full h-full row-span-2 object-cover`}
@@ -24,41 +24,41 @@ const ProjectDetail = () => {
             alt=""
           />
         </div>
-        <div className={`${styles.flexCol} gap-14`}>
-          <div className={`flex items-end gap-4`}>
+        <div className={`${styles.flexCol} gap-10 md:gap-14`}>
+          <div className={`${styles.flexResponsiveLarge} lg:items-end gap-6 lg:gap-4`}>
             <h2
               className={`${fontStyles.h2} font-bold text-blue flex-1 leading-tight`}
             >
               Lorem ipsum dolor sit amet consectetur.
             </h2>
-            <div className="basis-1/2 gap-y-2 grid grid-cols-2">
-              <div className={`${styles.flexRow} w-fit gap-16`}>
+            <div className="basis-1/2 gap-y-5 sm:gap-y-2 gap-x-10 grid grid-cols-1 sm:grid-cols-2 ">
+              <div className={`${styles.flexRow} gap-2 gap-5 lg:gap-16`}>
                 <Para1>Year:</Para1>
-                <Para1 color="font-blue" fontWeight="font-semibold">
+                <Para1 color="font-blue" fontWeight="font-semibold" style="w-max">
                   0000
                 </Para1>
               </div>
-              <div className={`${styles.flexRow} w-fit gap-16`}>
+              <div className={`${styles.flexRow}  gap-2 gap-5 gap-16`}>
                 <Para1>Category:</Para1>
-                <Para1 color="font-blue" fontWeight="font-semibold">
+                <Para1 color="font-blue" fontWeight="font-semibold"  style="w-max">
                   Lorem
                 </Para1>
               </div>
-              <div className={`${styles.flexRow} w-fit gap-16`}>
+              <div className={`${styles.flexRow}  gap-2 gap-5 gap-16 `}>
                 <Para1>Location:</Para1>
-                <Para1 color="font-blue" fontWeight="font-semibold">
+                <Para1 color="font-blue" fontWeight="font-semibold" style="w-max">
                   Lorem, Epsum
                 </Para1>
               </div>
-              <div className={`${styles.flexRow} w-fit gap-16`}>
+              <div className={`${styles.flexRow} gap-2 gap-5 gap-16`}>
                 <Para1>Designer:</Para1>
-                <Para1 color="font-blue" fontWeight="font-semibold">
+                <Para1 color="font-blue" fontWeight="font-semibold"  style="w-max">
                   Lorem Epsum
                 </Para1>
               </div>
             </div>
           </div>
-          <div className={`flex [&>div]:basis-1/2 gap-2`}>
+          <div className={`${styles.flexResponsiveLarge} lg:[&>div]:basis-1/2 gap-10 lg:gap-2`}>
             <div className={`${styles.flexCol} gap-6`}>
               <h3 className={`${fontStyles.h3} font-bold text-blue`}>
                 Overview
